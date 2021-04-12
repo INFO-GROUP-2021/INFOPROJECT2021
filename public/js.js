3a6d0d1c7006433498e7813ec9c2fb786c51c889
TheGreatDevourer <77646155+TheGreatDevourer@users.noreply.github.com>
2021-04-11 23:16:56 -0400

    v1.4

--- public/js.js
+++ public/js.js
@@ -1,14 +1,10 @@
           const tabs = M.Tabs.init(document.querySelector('.tabs'));
           const API_KEY = "1d611e5252ee50d950f35b8c75d928fc";
 
-          
-
-        const SearchBar = document.getElementById('searchBar');
-        SearchBar.addEventListener('keyup', (e)=> {
-          var searchString = e.target.value;
-          //searchBy(searchString);
-          console.log(searchString);
-        });
+          document.addEventListener('DOMContentLoaded', function() {
+            const elems = document.querySelectorAll('.collapsible');
+            const instances = M.Collapsible.init(elems, options);
+          });
 
           var options = {
               enableHighAccuracy: true,
@@ -21,30 +17,12 @@
   
               let URL = "https://api.openweathermap.org/data/2.5/onecall?lat="+crd.latitude+"&lon="+crd.longitude+"&exclude =alerts,minutely&units=metric&appid="+API_KEY;
   
-              let response = await fetch(URL);
+          let response = await fetch(URL);
               let data = await response.json();
               console.log(data);
               displayWeather(data);
             }
   
-            
-  
-            async function initSearch(){
-              var myHeaders = new Headers();
-              let data;
-              const getData ={
-                method:'GET',
-                headers: myHeaders,
-                mode: 'no-cors',
-                redirect: 'follow',
-                body: JSON.stringify(data)
-              };
-              const returnCityData = await fetch('current_city_list.json',getData)
-              const myData = await returnCityData;
-              console.log(myData);
-            }
-
-            initSearch();
 
   
             function displayWeather(data){
@@ -53,125 +31,130 @@
               let d2 =  new Date(data.current.sunset * 1000).toLocaleTimeString("en-US");
               mani. innerHTML = '';
               mani.innerHTML += ` 
-              <thead>
-              <th>Location</th>
-              </thead>
-              <tr>
-                <td>Longditute: ${data.lon}</td>
-                <td>Latitude: ${data.lat}</td>
-              </tr>
+              <li>
+              <div class="collapsible-header">
+                <h6>Location</h6>
+              </div>
+              <div class="collapsible-body white">
+                <p> Longditute: ${data.lon} </p>
+                <p>Latitude: ${data.lat}</p>
+              </div>
+              </li>
 
-              <thead>
-              <th> Current Weather Conditons</th>
-              </thead>
-                <tr>
-                    <td>Currently ${data.current.weather[0].description}</td>
-                    <td>Cloudy Percentage ${data.current.clouds}</td>
-                    <td>Humdity ${data.current.humidity}</td>
-                    <td>Wind Speed ${data.current.wind_speed}</td>
-                </tr>
-              
-                <thead>
-              <th>Temperture</th>
-              </thead>
-              <tr>
-                <td>Current Temperture: ${data.current.temp} C</td>
-                <td>Feels Like: ${data.current.feels_like} C</td>
-              </tr>
+              <li>
+              <div class="collapsible-header">
+              <h6> Current Weather Conditons</h6>
+              </div>
+                <div class="collapsible-body white">
+                    <p>Currently ${data.current.weather[0].description}</p>
+                    <p>Cloudy Percentage ${data.current.clouds}</p>
+                    <p>Humdity ${data.current.humidity}</p>
+                    <p>Wind Speed ${data.current.wind_speed}</p>
+                </div>
+                </li>
+              <li>
+            <div class="collapsible-header">
+              <h6>Temperture</h6>
+            </div>
+              <div class="collapsible-body white">
+                <p>Current Temperture: ${data.current.temp} C</p>
+                <p>Feels Like: ${data.current.feels_like} C</p>
+            </div>
+            </li>
+            <li>
+              <div class="collapsible-header">
+              <h6>UV Index/Sunrise and Set</h6>
+          </div>
 
-              <thead>
-              <th>UV Index/Sunrise and Set</th>
-              </thead>
-
-              <tr>
-                <td>Sunrise: ${d}</td>
-                <td>Sunset: ${d2}</td>
-                <td>UV Index ${data.current.uvi}</td>
-              </tr>  
+              <div class="collapsible-body white">
+                <p>Sunrise: ${d}</p>
+                <p>Sunset: ${d2}</p>
+                <p>UV Index ${data.current.uvi}</p>
+              </div>
+              </li>  
               `;
               display24(data);
             }
+
+         
 
             function display24(passed){
               datas = passed.hourly;
               let mani = document.querySelector("#results-24");
               mani.innerHTML='';
+              var i = 0;
               for(let data of datas){
                 let d = new Date(data.dt * 1000).toLocaleTimeString("en-US");
               mani.innerHTML += ` 
-              <thead>
-                <th>Hour: ${d}</th>
-              </thead>
-
-              <thead>
-                <th>Forecast</th>
-              </thead>
-                <tr>
-                <td>Weather  ${data.weather[0].description}</td>
-                <td> Cloudness Percentage ${data.clouds}</td>
-                <td>Humidity: ${data.humidity}</td>  
-                <td> Probability of Rain ${data.pop*100}%</td>
-                <td> Wind Speed ${data.wind_speed}</td>
-                </tr>
-                <thead>
-                <th>Temperture</th>
-              </thead>
-              <tr>
-                <td>Current Temperture: ${data.temp} C</td>
-                <td>Feels Like: ${data.feels_like} C</td>
-
-                <td> UV Index ${data.uvi}  </td> 
-              </tr>
-                <td> UV Index ${data.uvi}</td>   
-              </tr>
-           `
+              <li>
+              <div class="collapsible-header">
+                <h6>Hour: ${d}</h6>
+              </div>
+            <div class="collapsible-body white">
+                <h4> Forecast </h4>
+                <p>Weather  ${data.weather[0].description}</p>
+                <p> Cloudness Percentage ${data.clouds}</p>
+                <p>Humidity: ${data.humidity}</p>  
+                <p> Probability of Rain ${data.pop*100}%</p>
+                <p> Wind Speed ${data.wind_speed}</p>
+                <h4>Temperture</h4>
+                <p>Current Temperture: ${data.temp} C</p>
+                <p>Feels Like: ${data.feels_like} C</p>
+                <p> UV Index ${data.uvi}  </p> 
+              </div>
+            </li>
+           `;
+           i = i + 1;
             }
           
-            //createChart(datas);
-            display7Day(passed.daily);
+            
+            display7Day(passed);
           }
 
 
-          function display7Day(data){
+          function display7Day(passed){
+            data = passed.daily;
+            createChart(data);
             let mani = document.querySelector("#results-7D");
               mani. innerHTML = '';
-
-              for(let d of data){
+              let today = new Date();
+              for(let d of data){ 
                 let d1 = new Date(d.sunrise * 1000).toLocaleTimeString("en-US");
                 let d2 =  new Date(d.sunset * 1000).toLocaleTimeString("en-US");
                 mani.innerHTML+= `
-                  <thead>
-                    <th>Prediction</th>
-                  </thead>
-                  <td>Description: ${d.weather[0].description}</td>
-                  <td> Cloudy Percentage: ${d.clouds}</td>
-                  <td> Probablity of Rain: ${d.pop * 100}</td>
-                  <td> Humidty: ${d.humidity}</td>
-                  <thead>
-                  <th>Details</th>
-                  </thead>
-                  <td> Sunrise: ${d1}</td>
-                  <td>Sunset:  ${d2}</td>
-                  <td> UV Index: ${d.uvi}</td>
-                  <thead>
-                  <th>Tempertures</th>
-                  </thead>
-                  <tr>
-                  <td> Morning Temp ${d.temp.morn}</td>
-                  <td> Mid-day Temp ${d.temp.day}</td>
-                  <td> Evening ${d.temp.night}</td>
-                  <td> Night ${d.temp.eve}</td>
-                  </tr>
-                  </tr>
+                <li>
+                <div class="collapsible-header">
+                    <h6>Prediction: ${today}</h6>
+                </div>
+                <div class="collapsible-body white">
+                  <p>Description: ${d.weather[0].description}</p>
+                  <p> Cloudy Percentage: ${d.clouds}</p>
+                  <p> Probablity of Rain: ${d.pop * 100}</p>
+                  <p> Humidty: ${d.humidity}</p>
+                  <h3>Details</h3>
+                  
+                  <p> Sunrise: ${d1}</p>
+                  <p>Sunset:  ${d2}</p>
+                  <p> UV Index: ${d.uvi}</p>
+                  <h3>Tempertures</h3>
+                  <p> Morning Temp ${d.temp.morn}</p>
+                  <p> Mid-day Temp ${d.temp.day}</p>
+                  <p> Evening ${d.temp.night}</p>
+                  <p> Night ${d.temp.eve}</p>
+                  </div>
+                  </li>
                 `
+                today.setDate(today.getDate() +1);
+                
               }
+
           }
 
 
           
             function error(err) {
               console.warn(`ERROR(${err.code}): ${err.message}`);
-            }
+        }
           
             function locate(){
               navigator.geolocation.getCurrentPosition(success, error, options);
@@ -183,20 +166,27 @@
             function createChart(datas){
 
               const labels = [
-                '0','1','2','3','4','5','6','7','8','9','10',
-                '11'
+               '10', '20', '30', '40', '50','60','70', '80', '90','100'
               ];
+              let result = 0;
+
+          for (let d of datas){
+                result += (d.day + d.night + d.eve+ d.morning)/7;
+              }
+              console.log(result);
+
               const data = {
                 labels: labels,
                 datasets: [{
-                  label: 'Humidty Index',
-                  backgroundColor: 'rgb(255,255,255)',
-                  borderColor: 'rgb(255,255,255)',
-                  data: d }]};
+              label: 'Proibility of Perciptaion ',
+                  backgroundColor: 'rgba(0,0,0,1)',
+                  pointHoverBackgroundColor: 'rgba(255,255,255,1)',
+                  borderColor: 'rgba(0,0,0,1)',
+                  data : result}]};
 
               const config = {
                 type: 'line',
-                data,
+                data: data,
                 options: {}
               };
               
@@ -206,5 +196,7 @@
               );
 
             }
+
+        
        
          
